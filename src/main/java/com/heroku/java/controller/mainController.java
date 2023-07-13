a8466fae16fb836c8787000ccde5e53b37a51c5c
Aaarifs <2021898834@student.uitm.edu.my>
2023-07-14 06:08:19 +0800

    final

--- src/main/java/com/heroku/java/controller/mainController.java
+++ src/main/java/com/heroku/java/controller/mainController.java
@@ -131,6 +131,22 @@ public class mainController {
         return "admin/register-patient";
     }
 
+        @GetMapping("/staff")
+    public String staffs() {
+        // model.addAttribute("user", model);
+        return "admin/staff";
+    }
+
+        @GetMapping("/register-staff")
+    public String registerS() {
+        // model.addAttribute("user", model);
+        return "admin/register-staff";
+    }
+        @GetMapping("/update-staff")
+    public String updateS() {
+        // model.addAttribute("user", model);
+        return "admin/update-staff";
+    }
     @PostMapping("/register-patient")
     public String adminregisterPatient(HttpSession session, @ModelAttribute("admin-patient") patient patient,
             drug drugs) {
